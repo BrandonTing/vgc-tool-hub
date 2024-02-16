93429575ffae536d9c3824d25826a693cfa44f79
Brandon Ting <brandon1221t@gmail.com>
2024-02-16 09:51:37 +0800

    init radix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+import { Button } from "@radix-ui/themes";
 import { css } from "../../styled-system/css";
 
 export default function Home() {
-  return (
-    <div className={css({ fontSize: "2xl", fontWeight: 'bold', color: "red.200" })}>Hello 🐼!</div>
-    );
+	return (
+		<div
+			className={css({ fontSize: "2xl", fontWeight: "bold", color: "red.200" })}
+		>
+			Hello 🐼!
+			<Button>Let's go</Button>
+		</div>
+	);
 }
