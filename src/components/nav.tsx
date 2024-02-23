c4ec9e5a1e909701366fe83fb186a2344c9aa610
Brandon Ting <brandon1221t@gmail.com>
2024-02-23 15:49:50 +0800

    show active nav item

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -4,9 +4,50 @@
  * Documentation: https://v0.dev/docs#integrating-generated-code-into-your-nextjs-app
  */
 import { css } from "@/styled-system/css";
+import { headers } from "next/headers";
 import Link from "next/link";
 
+type Path = {
+	label: string;
+	url: `/${string}`;
+};
+
+function NavLink({
+	url,
+	isActive,
+	label,
+}: Path & {
+	isActive: boolean;
+}) {
+	return (
+		<Link
+			className={css({
+				color: "gray.900",
+				"&:hover": {
+					color: "gray.500",
+				},
+				_active: {
+					color: "blue.600",
+				},
+			})}
+			href={url}
+			data-active={isActive}
+		>
+			{label}
+		</Link>
+	);
+}
+
+const paths: Array<Path> = [
+	{
+		label: "Team Check",
+		url: "/team-check",
+	},
+];
+
 export default function Nav() {
+	const headerList = headers();
+	const pathname = headerList.get("next-url") || "";
 	return (
 		<nav
 			// bg-gray-950 w-full py-4 md:py-6
@@ -19,17 +60,14 @@ export default function Nav() {
 				justifyContent: "center",
 			})}
 		>
-			<Link
-				className={css({
-					color: "gray.900",
-					"&:hover": {
-						color: "gray.500",
-					},
-				})}
-				href="/team-check"
-			>
-				Team Check
-			</Link>
+			{paths.map((path) => (
+				<NavLink
+					key={path.url}
+					url={path.url}
+					label={path.label}
+					isActive={path.url === pathname}
+				/>
+			))}
 		</nav>
 	);
 }
